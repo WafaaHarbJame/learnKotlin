b5d7bfd8260c255bc92eaf29d674da11fbf55df8
WafaaHarbJame <49291280+WafaaHarbJame@users.noreply.github.com>
2024-05-19 21:44:55 +0300

    lesson 1

--- app/src/main/java/com/learnKotlin/learnKotlin/lesson1/Lesson1.kt
+++ app/src/main/java/com/learnKotlin/learnKotlin/lesson1/Lesson1.kt
@@ -6,10 +6,34 @@ class Lesson1 {
     //  difference between char and string
     // readLine command
     // $ and ${}
-    // string,double,float,char for one character ,boolean
+    // kotlin datatype  string,double,float,char for one character ,boolean
     // addition,subtraction,multiplication, conversion type
     // creating function  and fun name is small
     // null safety ? to allow null
-    //
+
+    fun getName(name:String?){
+
+    }
+    fun getData(){
+        val fName="wafaa"
+        val lName="Jame"
+        val age=50
+        println("the name$fName")
+        println("the last name$lName")
+        println("the age$age")
+    }
+    fun getAge(a:Int,b:Int): Unit {
+        val result=a+b
+        println("the result $result")
+
+    }
+    fun main(){
+        getData()
+        getAge(b = 7,a=6)
+        getAge(8,9)
+
+    }
+
+
 
 }
---
+++ app/src/main/java/com/learnKotlin/learnKotlin/lesson1/Main.kt
@@ -0,0 +1,15 @@
+package com.learnKotlin.learnKotlin.lesson1
+
+fun main(args:Array<String>){
+    getData()
+
+}
+
+fun getData(){
+    val fName="wafaa"
+    val lName="Jame"
+    val age=50
+    println("the name$fName")
+    println("the last name$lName")
+    println("the age$age")
+}
